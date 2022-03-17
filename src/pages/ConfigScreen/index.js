bc3faa867f6eb3f9a6cc7c64536f4e54af7456eb
Mohammed Uabidul Islam <MOHAMMED.U.ISLAM@stu.mmu.ac.uk>
2022-03-17 18:19:09 +0000

    Added Server Management buttons to ConfigScreen

--- src/pages/ConfigScreen/index.js
+++ src/pages/ConfigScreen/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Avatar, Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
+import { Avatar, Button, Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components'
 
 import ToggleThemeButton from '../../components/buttons/ToggleThemeButton'
+import baseURL from '../../resources/baseURL'
 
 const BackIcon = (props) => (
     <Icon {...props} name='arrow-back' />
@@ -18,6 +19,27 @@ export const ConfigScreen = ({ navigation }) => {
         <TopNavigationAction icon={BackIcon} onPress={navigateBack} />
     )
 
+    const serverManagement = async (type) => {
+        return fetch(baseURL + type, {
+            method: 'POST'
+        })
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json()
+                } else if (response.status === 400) {
+                    throw Error('Failed validation')
+                } else {
+                    throw Error('Something went wrong')
+                }
+            })
+            .then((responseJson) => {
+                console.log('Server: ', responseJson)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
     return (
         <>
             <SafeAreaView style={styles.safeAreaView}>
@@ -27,6 +49,9 @@ export const ConfigScreen = ({ navigation }) => {
                     <Avatar style={styles.avatar} size='large' source={require('../../../assets/icon.png')} />
                     <Text>Click the button to change theme</Text>
                     <ToggleThemeButton />
+                    <Button onPress={() => serverManagement('reset')}>Reset Server</Button>
+                    <Button onPress={() => serverManagement('resample')}>Resample</Button>
+
                 </Layout>
             </SafeAreaView>
         </>
